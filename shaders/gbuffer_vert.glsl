8e239138b253282fb1d6291b72c6ad498a7e3c09
Ben Harling <benjamin.harling@hotmail.com>
2016-02-07 21:55:22 +0000

    logarithmic depth buffer

--- shaders/gbuffer_vert.glsl
+++ shaders/gbuffer_vert.glsl
@@ -4,15 +4,23 @@ attribute vec2 aVertexTextureCoords;
   
   uniform mat4 uWorldViewProjectionMatrix;
   uniform mat3 uNormalMatrix;
+  uniform float nearClip;
+  uniform float farClip;
+  
   
   varying vec2 vTexCoords;
-  //varying vec2 depthClipSpace;
+
   varying vec3 vNormal;
   varying vec3 vEyeDirection;
   
   void main (void) {
+      const float C = 1.0;
+
       vTexCoords = aVertexTextureCoords;
       gl_Position = uWorldViewProjectionMatrix * vec4(aVertexPosition, 1.0);
+
+      gl_Position.z = log(C* gl_Position.z + 1.0) / log(C*farClip + 1.0) * gl_Position.w;
+      //gl_Position.z = log(gl_Position.z / nearClip) / log(farClip / nearClip);
       //depthClipSpace = gl_Position.zw;
       
       vNormal = uNormalMatrix * aVertexNormal;
